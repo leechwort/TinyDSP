ad36b2d9223c12a639de9d34518e2c4bc98e8f04
Artem Synytsyn <sincoon@gmail.com>
2019-06-27 13:16:27 +0300

    Added comments and license

--- tinydsp.c
+++ tinydsp.c
@@ -1,3 +1,10 @@
+/*
+ *  Copyright (c) 2019, Artem Synytsyn <a.synytsyn@gmail.com>
+ *  All rights reserved.
+ *
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *  See COPYING file for more information.
+ */
 #include "tinydsp.h"
 #include <stdio.h>
 #include <math.h>

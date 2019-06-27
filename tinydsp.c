e9386948e107fc8d5aadf217c410373bd2491e7c
Artem Synytsyn <sincoon@gmail.com>
2019-06-27 12:13:07 +0300

    Inverse FFT support and test added

--- tinydsp.c
+++ tinydsp.c
@@ -7,6 +7,7 @@
 tdsp_result tdsp_rfft_init(tdsp_rfft_cfg *cfg, unsigned int fftsize, bool is_ifft)
 {
   cfg->is_ifft = is_ifft;
+  cfg->fftsize = fftsize;
 #if defined(USE_KISSFFT)
 	cfg->cfg = kiss_fftr_alloc(fftsize, is_ifft, NULL, NULL);
 #elif defined(USE_ARMDSP)
@@ -22,7 +23,14 @@ tdsp_result tdsp_rfft_init(tdsp_rfft_cfg *cfg, unsigned int fftsize, bool is_iff
 tdsp_result tdsp_rfft_perform(tdsp_rfft_cfg *cfg, float *input_data, float *output_data)
 {
 #if defined(USE_KISSFFT)
-	kiss_fftr(cfg->cfg, (const kiss_fft_scalar* )&input_data[0], (kiss_fft_cpx *)&output_data[0]);
+  if (cfg->is_ifft) {
+    kiss_fftri(cfg->cfg, (const kiss_fft_scalar* )&input_data[0], (kiss_fft_cpx *)&output_data[0]);
+    /* Output data should be normalized to the window size, as is done in ARM version */
+    for (int i = 0; i < cfg->fftsize; i++)
+      output_data[i]  /= cfg->fftsize;
+  } else {
+      kiss_fftr(cfg->cfg, (const kiss_fft_scalar* )&input_data[0], (kiss_fft_cpx *)&output_data[0]);
+  }
 #elif defined(USE_ARMDSP)
 	arm_rfft_fast_f32(&cfg->S, input_data, output_data, cfg->is_ifft);
 #endif
